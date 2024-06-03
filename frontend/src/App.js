539696a61575b542b4d0235707d2735ed5c73173
Vaibhav <sahusv4527@gmail.com>
2024-06-03 17:23:46 +0530

    setting up

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from '@chakra-ui/button'
+import Homepage from './pages/homePage';
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme = "blue">button</Button>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/chatpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const chatpage = () => {
+  return (  
+    <div>
+    <h1>Chat Page</h1>
+    </div>
+    )
+}
+export default chatpage
